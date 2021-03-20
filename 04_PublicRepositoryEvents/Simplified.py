f7a0d84032081a9da8a4e21013a10d2a3af489a8
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2021-03-20 19:56:27 +0300

    04_PublicRepositoryEvents: added geometry string parsing

--- 04_PublicRepositoryEvents/Simplified.py
+++ 04_PublicRepositoryEvents/Simplified.py
@@ -3,8 +3,44 @@ from tkinter.constants import *
 from functools import partial
 from tkinter.messagebox import showinfo
 
+def parseAxisSettings(axis_str):
+    weight = 1
+    span = 1
+
+    span_ind = axis_str.find("+")
+    if (span_ind >= 0):
+        span = int(axis_str[span_ind+1 : ]) + 1
+        axis_str = axis_str[:span_ind]
+
+    weight_ind = axis_str.find(".")
+    if (weight_ind >= 0):
+        weight = int(axis_str[weight_ind+1 :])
+        axis_str = axis_str[:weight_ind]
+
+    ind = int(axis_str)
+
+    return (ind, weight, span)
+
+
+def parseGeometryStr(geometry_str):
+    sticky = "NEWS"
+    gravity_ind = geometry_str.find("/")
+    if (gravity_ind >= 0):
+        sticky = geometry_str[gravity_ind + 1 :]
+        geometry_str = geometry_str[: gravity_ind]
+
+    axis_splitter_ind = geometry_str.find(":")
+    row_params = parseAxisSettings(geometry_str[:axis_splitter_ind])
+    column_params = parseAxisSettings(geometry_str[axis_splitter_ind+1 :])
+
+    return (row_params, column_params, sticky)
+
 def setGeometry(child, geometry_str):
-    print(geometry_str)
+    row_params, column_params, sticky = parseGeometryStr(geometry_str)
+    child.grid(row=row_params[0], rowspan=row_params[2],
+               column=column_params[0], columnspan=column_params[2], sticky=sticky)
+    child.master.grid_rowconfigure(row_params[0], weight=row_params[1])
+    child.master.grid_columnconfigure(column_params[0], weight=column_params[1])
 
 def createWidget(master, widget_name, widget_type, geometry_str, **kwargs):
     class CustomWidget(widget_type):
@@ -22,6 +58,9 @@ class Application(tk.Frame):
     def __init__(self, title):
         super(Application, self).__init__()
         self.master.title(title)
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        self.grid(sticky="NWSE")
         self.createWidgets()
 
     def createWidgets(self):
